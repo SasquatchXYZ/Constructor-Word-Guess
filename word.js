656e776017daf03feae28472d4f54db451e0220d
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-10-31 00:35:21 -0400

    WIP word.js working properly

--- word.js
+++ word.js
@@ -1,10 +1,10 @@
 const Letter = require('./letter');
 //let letter = new Letter();
 
-let Word = function(chosenWord) {
+let Word = function (chosenWord) {
     this.chosenWord = chosenWord;
     this.wordArray = [];
-    this.stringifyWord = function() {
+    this.makeWordArray = function () {
         let chosenWordArray = this.chosenWord.split('');
         console.log(chosenWordArray);
         for (let k = 0; k < chosenWordArray.length; k++) {
@@ -13,36 +13,42 @@ let Word = function(chosenWord) {
         }
         //console.log(this.wordArray);
 
-        this.wordArray.forEach(arrayLetter => {
+        /*this.wordArray.forEach(arrayLetter => {
             console.log(arrayLetter.renderChar());
-        });
-
+        });*/
+    };
+    this.stringifyWord = function () {
         const displayWord = this.wordArray.map(wordLetter => {
             return wordLetter.renderChar();
         });
+
         console.log(displayWord.toString().split(',').join(' '));
+        return displayWord.toString().split(',').join(' ');
 
-/*        this.wordArray.forEach(arrLetter => {
-            console.log(arrLetter.renderChar());
-            const fullWord = this.wordArray.map(wordLetter => {
-                return wordLetter.toString;
-            });
 
-            console.log(fullWord.join(''));
-        });*/
+        /*        this.wordArray.forEach(arrLetter => {
+                    console.log(arrLetter.renderChar());
+                    const fullWord = this.wordArray.map(wordLetter => {
+                        return wordLetter.toString;
+                    });
+
+                    console.log(fullWord.join(''));
+                });*/
 
 
         //console.log(this.wordArray);
-/*        for (let k = 0; k < this.wordArray.length; k++) {
-            if (!this.wordArray[k].guessed) {
-                console.log(this.wordArray[k].placeholder);
-            } else {
-                console.log(this.wordArray[k].toString);
-            }
-        }*/
+        /*        for (let k = 0; k < this.wordArray.length; k++) {
+                    if (!this.wordArray[k].guessed) {
+                        console.log(this.wordArray[k].placeholder);
+                    } else {
+                        console.log(this.wordArray[k].toString);
+                    }
+                }*/
     };
-    this.guess = function(character) {
-        this.wordArray.forEach(letter.checkGuess(character))
+    this.guess = function (character) {
+        this.wordArray.forEach(wordLetter => {
+            wordLetter.checkGuess(character)
+        })
     }
 };
 
@@ -50,4 +56,9 @@ let chosenWord = 'pangolin';
 let word = new Word(chosenWord);
 console.log(word.chosenWord);
 console.log(word.wordArray);
+word.makeWordArray();
+word.stringifyWord();
+word.guess("a");
+word.stringifyWord();
+word.guess("n");
 word.stringifyWord();
