0c18e1f87f799689f86526bbbd58a8fe6a664f98
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-11-01 03:58:03 -0400

    Working on improvements and working in other queries and npm modules

--- prompt.js
+++ prompt.js
@@ -1,5 +1,53 @@
 const prompt = require('prompt');
-const colors = require('colors/safe');
+const inquirer = require('inquirer');
+const colors = require('colors');
+const Word = require('./word');
+
+let guessesLeft;
+let usedWords;
+let word;
+let chosenWord;
+let playerName;
+
+function welcome() {
+    usedWords = [];
+    console.log(`Hello and Welcome to Command Line Hangman featuring ALL OF THE ANIMALS`.bold.cyan);
+    console.log(`----------------------------------------------------------------------`.rainbow);
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'What is your name?'.magenta,
+                name: 'username',
+            }
+        ])
+        .then(function (response) {
+            playerName = response.username;
+            console.log(`Welcome ${playerName}!`.bold.cyan);
+            startGame();
+        });
+}
+
+function startGame() {
+    chosenWord = '';
+    guessesLeft = 15;
+    //console.log(usedWords);
+    if (usedWords.length < wordsArray.length) {
+        chosenWord = wordSelector();
+    } else {
+        console.log(`It looks like you used up all the animals in the bank...`);
+        restartGame();
+    }
+    if (chosenWord) {
+        word = new Word(chosenWord);
+        word.makeWordArray();
+        promptGuesses();
+    }
+}
+
+
+
+
 /*
 prompt.start();
 prompt.get(['username','email'], function (err, result) {
@@ -8,7 +56,9 @@ prompt.get(['username','email'], function (err, result) {
     console.log(`email: ${result.email}`);
 });*/
 
-let schema = {
+
+
+/*let schema = {
     properties: {
         name: {
             description: colors.magenta('What is Your Name?'),
@@ -23,12 +73,16 @@ let schema = {
         }
     }
 };
-prompt.message = colors.rainbow("Question!");
-prompt.delimiter = colors.green("><");
+//prompt.message = colors.rainbow("Question!");
+prompt.message = colors.rainbow(`Hello and Welcome to Command Line Hangman featuring ALL OF THE ANIMALS`);
+prompt.message = colors.rainbow(`----------------------------------------------------------------------`);
+prompt.delimiter = colors.green("--><--");
 prompt.start();
 
 prompt.get(schema, function(error, result) {
     console.log('command-line input received:');
     console.log(`name: ${result.name}`);
     console.log(`password: ${result.password}`);
-});
+});*/
+
+welcome();
